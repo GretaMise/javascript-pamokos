ba2e26255ea40185bcfb0db36cdcbcd3e79f1bdb
GretaMise <greta990521@gmail.com>
2024-10-30 21:44:54 +0200

    Initial commit

---
+++ pirma-pamoka/script.js
@@ -0,0 +1,18 @@
+console.log(2);
+//   isvalo viska iki sio kodo
+console.clear();
+console.log("Greta");
+console.error("Something is wrong");
+console.count(100);
+//   Math
+console.log(4 + 11);
+console.log(1 > 2);
+//   table
+console.table(["apple", "banana", "orange"]);
+//   warn, ispeja
+console.warn("Ispejimas");
+//   time (padeda pamatuoti kiek laiko uztrunka vykdyti koda nuo time iki timeend apskaiciuoja, del to eina kartu visad sie kodai)
+console.time();
+console.timeEnd();
+
+// document.getElementById("heading").innerHTML = "Hello World!";
