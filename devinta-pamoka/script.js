6ce3153166c6fc91a59832874e24616e52a0e493
GretaMise <greta990521@gmail.com>
2024-11-18 21:53:47 +0200

    add devinta pam

--- devinta-pamoka/script.js
+++ devinta-pamoka/script.js
@@ -93,3 +93,26 @@ for (let i = 2; i < count; i++) {
   first = second;
   second = sum4;
 }
+
+// Ciklai - while
+// Vykdome skaičiavimus ir norime juos vykdyti iki kol pasieksime tinkamą rezultatą.
+
+let skaicius = 1;
+while (skaicius <= 5) {
+  console.log(skaicius);
+  skaicius++;
+}
+
+let tekstas = 'Hello world!';
+let kiekLiko = 10;
+
+while (kiekLiko > 0) {
+  console.log(tekstas);
+  kiekLiko--;
+}
+
+let prekiuKiekis = 30;
+while (prekiuKiekis > 0) {
+  console.log(`Prekiu likutis: `, prekiuKiekis);
+  prekiuKiekis--;
+}
