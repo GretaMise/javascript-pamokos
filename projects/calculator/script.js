d5d833d89aeef01618a1a1fb867cf92f81e7dd55
GretaMise <greta990521@gmail.com>
2025-01-08 18:57:26 +0200

    Add

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,10 +1,11 @@
 let firstNumber = 0;
-let secondNumber = 0;
 let action = '+';
 let answer = 0;
 
 let input = document.getElementById('calc-input');
 let calculation = document.getElementById('calculation');
+
+let history = [];
 
 function onNumberClick(number) {
   input.value += number;
@@ -16,45 +17,48 @@ function onActionClick(clickedAction) {
 }
 
 function onEqualClick() {
-  // 5 * 2
-  let splitted = input.value.split(' ');
-  //   ['5', '*', '2']
-  //   5
-  firstNumber = parseInt(splitted[0]);
-  //   *
-  action = splitted[1];
-  //   2
-  secondNumber = parseInt(splitted[2]);
-  //   iskvieciame skaiciavimo funkcija
-  calculateAnswer();
-  input.value = answer;
+  let splitted = input.value.split(' ').map((part) => part.trim());
 
-  calculation.innerHTML = `${firstNumber} ${action} ${secondNumber}`;
+  if (splitted[0] === '' || splitted[0] === '0') {
+    alert('The first number cannot be zero.');
+    return;
+  }
+
+  let numbers = splitted.map((num) => parseFloat(num.replace(',', '.')));
+
+  calculateAnswer(numbers);
+
+  input.value = answer.toFixed(2).replace('.', ',');
+  calculation.innerHTML = `${numbers.join(` ${action} `)} = ${answer
+    .toFixed(2)
+    .replace('.', ',')}`;
 }
 
-function calculateAnswer() {
-  // action = '+' arba dalybai arba minusui...
-  switch (action) {
-    case '+':
-      answer = firstNumber + secondNumber;
-      break;
-    case '-':
-      answer = firstNumber - secondNumber;
-      break;
-    case '*':
-      answer = firstNumber * secondNumber;
-      break;
-    case '/':
-      answer = firstNumber / secondNumber;
-      break;
+function calculateAnswer(numbers) {
+  answer = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    switch (action) {
+      case '+':
+        answer += numbers[i];
+        break;
+      case '-':
+        answer -= numbers[i];
+        break;
+      case '*':
+        answer *= numbers[i];
+        break;
+      case '/':
+        answer /= numbers[i];
+        break;
+    }
   }
 }
 
 function onClearCalculator() {
-  let firstNumber = 0;
-  let secondNumber = 0;
-  let action = '+';
-  let answer = 0;
-  input.value = ' ';
+  firstNumber = 0;
+  action = '+';
+  answer = 0;
+  input.value = '';
   calculation.innerHTML = '';
 }
