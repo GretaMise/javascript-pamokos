71992a541c2e9e0bd5b5989265f7ae09e0d6f58e
GretaMise <greta990521@gmail.com>
2025-01-18 23:13:15 +0200

    add 30

--- 30-api/script.js
+++ 30-api/script.js
@@ -5,14 +5,22 @@ let debounceTimer;
 
 const fetchProducts = async (query = '') => {
   const api = 'https://fakestoreapi.com/products';
-  // const searchApi = `https://fakestoreapi.com/search/?q=${query}`;
+  const searchApi = `https://fakestoreapi.com/search/products?q=${query}`;
   const url = query ? searchApi : api;
   try {
     const response = await fetch(url);
     const data = await response.json();
+    // const filteredProducts = query
+    //   ? data.filter((product) =>
+    //       product.title.toLowerCase().includes(query.toLowerCase())
+    //     )
+    //   : data;
+    // displayProducts(filteredProducts);
     displayProducts(query ? data : data.map((product) => ({ product })));
+    // (data);
+    // (query ? data : data.map((product) => ({ product })));
   } catch (error) {
-    console.error(error);
+    console.error('Error :((');
   }
 };
 
@@ -31,6 +39,7 @@ const displayProducts = (products) => {
       <img src="${product.image}" alt="produkto nuotrauka">
       <h3>${product.title}</h3>
       <p>Price: ${product.price}</p>
+      <button>Perziureti</button>
     `;
 
     productsContainer.appendChild(productCard);
@@ -47,3 +56,7 @@ searchInput.addEventListener('input', (event) => {
 });
 
 fetchProducts();
+
+// const products = await fetch('https://fakestoreapi.com/products')
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
