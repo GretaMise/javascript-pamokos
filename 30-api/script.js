36c0de1cc67c28fa6a815b584bd50aa8b50a8438
GretaMise <greta990521@gmail.com>
2025-01-18 18:56:41 +0200

    add 30

--- 30-api/script.js
+++ 30-api/script.js
@@ -1,1 +1,49 @@
 // REST API - dazniausiai naudosim
+const searchInput = document.getElementById('searchInput');
+const productsContainer = document.getElementById('productsContainer');
+let debounceTimer;
+
+const fetchProducts = async (query = '') => {
+  const api = 'https://fakestoreapi.com/products';
+  // const searchApi = `https://fakestoreapi.com/search/?q=${query}`;
+  const url = query ? searchApi : api;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProducts(query ? data : data.map((product) => ({ product })));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const displayProducts = (products) => {
+  productsContainer.innerHTML = '';
+  if (!products.length) {
+    productsContainer.innerHTML = '<p>Produktu nerasta</p>';
+    return;
+  }
+
+  products.forEach((item) => {
+    const product = item.product;
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    productCard.innerHTML = `
+      <img src="${product.image}" alt="produkto nuotrauka">
+      <h3>${product.title}</h3>
+      <p>Price: ${product.price}</p>
+    `;
+
+    productsContainer.appendChild(productCard);
+  });
+};
+
+searchInput.addEventListener('input', (event) => {
+  const inputValue = event.target.value;
+
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    fetchProducts(inputValue);
+  }, 500);
+});
+
+fetchProducts();
